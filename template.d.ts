639e847081dd52a0fba311fcb7e32e7a8a372ed4
Emiya <emiya@emiya.com.cn>
2024-01-09 02:36:58 +0800

    chore

--- template.d.ts
+++ template.d.ts
@@ -1,8 +1,6 @@
-// ./template.d.ts
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			// DON'T CHANGE NEXT LINE, this will be replaced by the env variables
 			TEMPLATE_REPLACE
 		}
 	}
